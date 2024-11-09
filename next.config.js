3de28179c5b31ee9cef504c5127f54e98e31065c
phinahasphilip2000@gmail.com <phinahasphilip2000@gmail.com>
2024-11-09 19:46:26 +0530

    Testing production build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{appDir: true}
-}
+    experimental: { appDir: true },
+    output: 'standalone', // Enables server-rendering mode
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
