7f9eb681791ba60edd9325f8ac81c44d5a2291fb
phinahasphilip2000@gmail.com <phinahasphilip2000@gmail.com>
2024-11-09 19:33:01 +0530

    Production bug fix

--- src/app/product/[productId]/page.js
+++ src/app/product/[productId]/page.js
@@ -1,12 +1,21 @@
+import ProductScreen from '../../../screens/Product/ProductScreen';
 
-import ProductScreen from '../../../screens/Product/ProductScreen'
+// Define the dynamic routes to pre-render
+export async function generateStaticParams() {
+
+  const productIds = await fetchProductIds();
+
+  return productIds.map((id) => ({
+    productId: id.toString(), 
+  }));
+}
 
 const ProductPage = ({ params }) => {
   const { productId } = params;
 
   return (
-    <ProductScreen producId={productId} />
-  )
+    <ProductScreen productId={productId} />
+  );
 };
 
 export default ProductPage;
